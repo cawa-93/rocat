dece47e8754bd400cb60eab6534fd721a4ce35e6
Олександр Козак <kozackunisoft@gmail.com>
2024-01-25 21:22:13 +0200

    debug

--- src/main.js
+++ src/main.js
@@ -5,6 +5,15 @@ import App from './App.vue'
 
 const supportedLocales = ['en', 'uk']
 const allowedLocales = navigator.languages.filter(l => supportedLocales.includes(l))
+function l (...args) {
+    alert(JSON.stringify(args))
+}
+
+l({'navigator.languages': navigator.languages})
+l({supportedLocales})
+l({allowedLocales})
+l({'navigator.language': navigator.language})
+
 const i18n = createI18n({
     locale: allowedLocales[0],
     fallbackLocale: supportedLocales[0]
